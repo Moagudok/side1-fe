2fd81fe921321fee5dc5ddcf8adf18d605351629
Sunjin Baek <tjswls5000@gmail.com>
2022-11-17 17:30:55 +0900

    feat: add products pages

---
+++ app/routes/products/index.tsx
@@ -0,0 +1,33 @@
+import type { LoaderFunction } from "@remix-run/node";
+import { json } from "@remix-run/node";
+import { useLoaderData, Link } from "@remix-run/react";
+import type { Joke } from "@prisma/client";
+
+import { db } from "~/utils/db.server";
+
+type LoaderData = { randomJoke: Joke };
+
+export const loader: LoaderFunction = async () => {
+  const count = await db.joke.count();
+  const randomRowNumber = Math.floor(Math.random() * count);
+  const [randomJoke] = await db.joke.findMany({
+    take: 1,
+    skip: randomRowNumber,
+  });
+  const data: LoaderData = { randomJoke };
+  return json(data);
+};
+
+export default function JokesIndexRoute() {
+  const data = useLoaderData<LoaderData>();
+
+  return (
+    <div>
+      <p>Here's a random joke:</p>
+      <p>{data.randomJoke.content}</p>
+      <Link to={data.randomJoke.id}>
+        "{data.randomJoke.name}" Permalink
+      </Link>
+    </div>
+  );
+}
